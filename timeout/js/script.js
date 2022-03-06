6b2b32df2aadb2a365c07f19c2378f59ddd40e5c
wudueuk <wudueuk@gmail.com>
2022-03-06 12:44:48 +0200

    Вторая бонусная задача, немного скорректированная

--- timeout/js/script.js
+++ timeout/js/script.js
@@ -3,12 +3,9 @@ import timer from './timer.js';
 const timerPlugin = () => {
   const allDiv = document.querySelectorAll('div');
 
-  let timerDiv;
-
   for (let i = 0; i < allDiv.length; i++) {
     if (allDiv[i].dataset.timerDeadline) {
-      timerDiv = allDiv[i];
-      timer(timerDiv);
+      timer(allDiv[i]);
     }
   }
 };
