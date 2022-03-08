3b25d2ae02c78978224eb55d5a55e0b7d4512435
wudueuk <wudueuk@gmail.com>
2022-03-08 17:20:15 +0200

    Урок 3, плюс доп. задание

--- timeout/js/modules/acc.js
+++ timeout/js/modules/acc.js
@@ -2,13 +2,21 @@ const items = document.querySelectorAll('.travel__item');
 const buttons = document.querySelectorAll('.travel__item-title');
 const wrappers = document.querySelectorAll('.travel__item-text-wrapper');
 
+let heightWrapper = 0;
+
+wrappers.forEach(elem => {
+  if (elem.scrollHeight > heightWrapper) {
+    heightWrapper = elem.scrollHeight;
+  }
+});
+
 buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
     for (let i = 0; i < items.length; i++) {
       if (index === i) {
         wrappers[i].style.height =
           items[index].classList.contains('travel__item_active') ?
-            '' : `${wrappers[i].scrollHeight}px`;
+            '' : `${heightWrapper}px`;
         items[i].classList.toggle('travel__item_active');
       } else {
         items[i].classList.remove('travel__item_active');
